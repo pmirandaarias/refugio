0dfade3e6463eb868bb62de2fbf50bdc5ad1d6d5
Paulo Miranda <pmirandaarias@gmail.com>
2017-05-12 18:53:18 -0300

    Se implementa Eliminar y Editar formularios

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
 from apps.adopcion.models import Persona, Solicitud
@@ -43,3 +43,44 @@ class SolicitudCreate(CreateView):
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
+
+class SolicitudUpdate(UpdateView):
+    model = Solicitud
+    second_model = Persona
+    template_name = "adopcion/solicitud_form.html"
+    form_class = SolicitudForm
+    second_form_class = PersonaForm
+    success_url = reverse_lazy('adopcion:solicitud_listar')
+
+    def get_context_data(self, **kwargs):
+        context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk', 0)
+        solicitud = self.model.objects.get(id=pk)
+        persona = self.second_model.objects.get(id=solicitud.persona_id)
+        if 'form' not in context:
+            context['form'] = self.form_class(self.request.GET)
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance=persona)
+            context['id'] = pk
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        id_solicitud = kwargs['pk']
+        solicitud = self.model.objects.get(id=id_solicitud)
+        persona = self.second_model.objects.get(id=solicitud.persona_id)
+        form = self.form_class(request.POST, instance=solicitud) # sino colocamos instance va a crear un nuevo objeto en vez de actualizar
+        form2 = self.second_form_class(request.POST, instance=persona) # sino colocamos instance va a crear un nuevo objeto en vez de actualizar
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return HttpResponseRedirect(self.get_success_url())
+
+
+class SolicitudDelete(DeleteView):
+    model = Solicitud
+    template_name = 'adopcion/solicitud_delete.html'
+    success_url = reverse_lazy('adopcion:solicitud_listar')
